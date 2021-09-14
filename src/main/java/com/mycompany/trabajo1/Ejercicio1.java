3c1e40d79ea3475c66c54e8b2a9dc8c2d3357f51
THINKPAD T470 <THINKPAD T470@DESKTOP-EMM16I3>
2021-09-14 12:47:54 -0500

    primer punto

---
+++ src/main/java/com/mycompany/trabajo1/Ejercicio1.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.trabajo1;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+       int numero1 = 12;
+       int numero2 = 2;
+       double division;
+       int suma,resta,multiplicacion;
+       double modulo;
+       double operacion;
+       
+       operacion = 9/3*5-3+8*2;
+        System.out.println("El resultado de la operacion es = " + operacion);
+        division = numero1/numero2;
+        System.out.println("El resultado de la division es = " + division);
+        suma = numero1+numero2;
+        System.out.println("El resultado de la suma es = " + suma );
+        resta = numero1-numero2;
+        System.out.println("El resultado de la resta es = " + resta);
+        multiplicacion = numero1*numero2;
+        System.out.println("El resultado de la multiplicacion es = " + multiplicacion);
+        modulo = numero1%numero2;
+        System.out.println("El resultado del modulo es = " + modulo);
+        
+    }
+    
+}
---
+++ src/main/java/com/mycompany/trabajo1/Ejercicio2.java
@@ -0,0 +1,17 @@
+package com.mycompany.trabajo1;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+  int N = 5;
+  double A = 4.56;
+  String C = "a";
+        System.out.println(" Variable N = " + N);
+        System.out.println(" Variable A = "+ A);
+        System.out.println(" Variable C = " + C);
+        System.out.println(N+" + " +A " = "+(N+A));
+        
+        
+        
+        
+    }
+    
+}
